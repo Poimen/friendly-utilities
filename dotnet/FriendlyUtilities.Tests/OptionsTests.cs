b06549d5b0280c2dff2d71ef77b5dca495d6ee49
Richard Shephard <633780+Poimen@users.noreply.github.com>
2025-08-25 08:48:50 +0200

    wip

---
+++ dotnet/FriendlyUtilities.Tests/OptionsTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace FriendlyUtilities.Tests;
+
+[TestFixture]
+public class OptionsTests
+{
+    private static readonly Random Random = new();
+
+    public class GivenAnOption
+    {
+        [Test]
+        public void WithoutAValueType_ShouldHaveNoValue()
+        {
+            var option = Option<int>.None;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(option.IsSome, Is.False);
+                Assert.That(option.IsNone, Is.True);
+            });
+        }
+
+        [Test]
+        public void WithAValueType_ShouldHaveValue()
+        {
+            var option = Option<int>.Some(Random.Next());
+            var o = Optional.Some(Random.Next());
+
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(option.IsSome, Is.True);
+                Assert.That(option.IsNone, Is.False);
+            });
+        }
+
+        public class Comparisons
+        {
+            [Test]
+            public void WithTheSameValue_ShouldEqual()
+            {
+                var value = Random.Next();
+                var option1 = Option<int>.Some(value);
+                var option2 = Option<int>.Some(value);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(option1, Is.EqualTo(option2));
+                });
+            }
+        }
+    }
+}
---
+++ dotnet/FriendlyUtilities/OptionType.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace FriendlyUtilities;
+
+public abstract record OptionType;
+
+public record Some<T>(T Value) : OptionType where T : notnull;
+public record None<T> : OptionType where T : notnull;
+
+public static class OptionalExtensions
+{
+    public static TResult Match<T, TResult>(this OptionType optionType, Func<T, TResult> some, Func<TResult> none) where T : notnull =>
+        optionType switch
+        {
+            Some<T> someValue => some(someValue.Value),
+            None<T> => none(),
+            _ => throw new InvalidOperationException("Unexpected option type.")
+        };
+}
+
+public static class Optional
+{
+    public static OptionType Some<T>(T value) where T : notnull => new Some<T>(value);
+}
+
+public class Option<T> : IEquatable<Option<T>>, IStructuralEquatable, IComparable<Option<T>>, IComparable, IStructuralComparable
+    where T : notnull
+{
+    private readonly T? _value;
+
+    public static Option<T> Some(T value) => new(value);
+    public static Option<T> None => new();
+
+    public bool IsSome { get; }
+    public bool IsNone => !IsSome;
+
+    private Option(T value)
+    {
+        _value = value;
+        IsSome = true;
+    }
+
+    private Option()
+    {
+        IsSome = false;
+    }
+
+    public bool Equals(OptionType? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Equals(Option<T>? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(Option<T>? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is OptionType other && Equals(other);
+
+    public int CompareTo(object? other, IComparer comparer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(OptionType? other) => CompareTo(other, Comparer<T>.Default);
+
+    public bool Equals(object? other, IEqualityComparer comparer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetHashCode(IEqualityComparer comparer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode() =>
+        GetHashCode(EqualityComparer<T>.Default);
+
+    public override string ToString() => IsSome ? $"Some({_value})" : "None";
+
+    public static bool operator ==(Option<T> a, Option<T> b) =>
+        a.Equals(b);
+
+    public static bool operator !=(Option<T> a, Option<T> b) =>
+        !(a == b);
+
+    public static bool operator <(Option<T> a, Option<T> b) =>
+        a.CompareTo(b) < 0;
+
+    public static bool operator >(Option<T> a, Option<T> b) =>
+        a.CompareTo(b) > 0;
+
+    public static bool operator <=(Option<T> a, Option<T> b) =>
+        a.CompareTo(b) <= 0;
+
+    public static bool operator >=(Option<T> a, Option<T> b) =>
+        a.CompareTo(b) >= 0;
+}
